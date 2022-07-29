04ee461ef6741a175b96b47b517dc26e02237cdd
Matej Holubec <matej.holubec@gmail.com>
2022-07-29 08:53:16 +0200

    feat: mai

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <span class="brambora">
+      🥔
+    </span> <br>  
+    <CountComp/>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import CountComp from '@/components/CountComp.vue'; // @ is an alias to /src
 
 @Options({
   components: {
-    HelloWorld,
+    CountComp,
   },
 })
 export default class HomeView extends Vue {}
 </script>
+<style lang="scss">
+.brambora {
+  font-size: 15rem;
+}
+</style>
